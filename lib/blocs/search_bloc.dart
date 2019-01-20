e99eba74dc7c4b72c0deae586c82e6ae8513f606
videah <videah@selfish.systems>
2019-01-20 20:26:34 +0000

    strip flash posts from search results

--- lib/blocs/search_bloc.dart
+++ lib/blocs/search_bloc.dart
@@ -6,17 +6,27 @@ import 'package:rxdart/rxdart.dart';
 import 'package:msg_browser/api/models/post_list_item.dart';
 
 class SearchBloc {
-
   final client = E621Client();
   List<PostListItem> _items;
   String _tags;
 
+  // Flash is a dead meme.
+  // TODO: maybe do this in the search query instead
+  _stripFlashPosts(List<PostListItem> posts) {
+    posts.removeWhere((f) => f.fileExt == "swf");
+    return posts;
+  }
+
   _handleSearch(String tags) async {
     _tags = tags;
     _searchSubject.add(_tags);
-    var response = await client.get("https://e621.net/post/index.json?tags=$tags");
+    var response =
+        await client.get("https://e621.net/post/index.json?tags=$tags");
     var decoded = json.decode(response.body);
-    _items = (decoded as List).map((i) => PostListItem.fromJson(i)).toList();
+
+    List<PostListItem> posts =
+        (decoded as List).map((i) => PostListItem.fromJson(i)).toList();
+    _items = _stripFlashPosts(posts);
     _itemsSubject.add(_items);
   }
 
@@ -25,7 +35,8 @@ class SearchBloc {
   }
 
   void _grabInitialData() async {
-    var response = await client.get("https://e621.net/post/index.json?limit=10");
+    var response =
+        await client.get("https://e621.net/post/index.json?limit=10");
     var decoded = json.decode(response.body);
     _items = (decoded as List).map((i) => PostListItem.fromJson(i)).toList();
     _itemsSubject.add(_items);
@@ -45,7 +56,6 @@ class SearchBloc {
     _searchSubject.close();
     _searchController.close();
   }
-
 }
 
 class SearchProvider extends InheritedWidget {
@@ -63,5 +73,4 @@ class SearchProvider extends InheritedWidget {
   static SearchBloc of(BuildContext context) =>
       (context.inheritFromWidgetOfExactType(SearchProvider) as SearchProvider)
           .bloc;
-
 }
