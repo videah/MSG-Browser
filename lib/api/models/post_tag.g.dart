c7eab346de081043e044684e53893aa018a97961
videah <videah@selfish.systems>
2019-01-22 22:27:33 +0000

    woops, forgot g file

---
+++ lib/api/models/post_tag.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'post_tag.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+PostTag _$PostTagFromJson(Map<String, dynamic> json) {
+  return PostTag(json['id'] as int, json['name'] as String, json['type'] as int,
+      json['type_locked'] as bool);
+}
+
+Map<String, dynamic> _$PostTagToJson(PostTag instance) => <String, dynamic>{
+      'id': instance.id,
+      'name': instance.name,
+      'type': instance.type,
+      'type_locked': instance.typeLocked
+    };
