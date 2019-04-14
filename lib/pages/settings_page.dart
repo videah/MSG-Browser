9309a014d0769a74a85a5cc8b8f7ce82205d33c5
videah <videah@selfish.systems>
2019-04-14 16:07:32 +0100

    small layout changes

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -14,68 +14,72 @@ class _SettingsPageState extends State<SettingsPage> {
   Widget build(BuildContext context) {
     var bloc = BlocProvider.of<SettingsBloc>(context);
     return Scaffold(
-        backgroundColor: Theme.of(context).canvasColor,
-        appBar: AppBar(
-          title: Text("Settings"),
-        ),
-        body: PersistedStateBuilder(
-          builder: (context, snapshot) {
-            if (!snapshot.hasData) return Container();
-            return ListView(
-              children: <Widget>[
-                CheckboxListTile(
-                  title: Text("IDGAF Mode"),
-                  activeColor: Colors.blue,
-                  subtitle: Text("Skip post pages and go directly to image viewer"),
-                  onChanged: (value) {
-                    setState(() {
-                      snapshot.data["idgaf"] = value;
-                    });
-                  },
-                  value: snapshot.data["idgaf"] ?? false,
-                ),
-                ListTile(
-                  title: Text("Flush image cache"),
-                  subtitle: ValueBuilder(
-                    streamed: bloc.cacheSize,
-                    builder: (context, snapshot) => Text("Flushes all images from the cache (${snapshot.data} Mb)"),
-                  ),
-                  leading: Icon(
-                    Icons.cached,
-                    size: 48.0,
-                  ),
-                  onTap: () {
-                    showDialog(
-                      context: context,
-                      builder: (context) {
-                        return AlertDialog(
-                          title: Text("Warning"),
-                          content: Text(
-                            "Are you sure you want to clear the image cache? This cannot be undone.",
-                          ),
-                          actions: <Widget>[
-                            FlatButton(
-                              child: Text("CANCEL"),
-                              onPressed: () {
-                                Navigator.of(context).pop();
-                              },
-                            ),
-                            FlatButton(
-                              child: Text("CLEAR"),
-                              onPressed: () {
-                                bloc.clearCache();
-                                Navigator.of(context).pop();
-                              },
-                            ),
-                          ],
-                        );
-                      },
+      backgroundColor: Theme.of(context).canvasColor,
+      appBar: AppBar(
+        title: Text("Settings"),
+      ),
+      body: PersistedStateBuilder(
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) return Container();
+          return ListView(
+            children: <Widget>[
+              CheckboxListTile(
+                title: Text("IDGAF Mode"),
+                activeColor: Colors.blue,
+                subtitle:
+                    Text("Skip post pages and go directly to image viewer"),
+                onChanged: (value) {
+                  setState(() => snapshot.data["idgaf"] = value);
+                },
+                value: snapshot.data["idgaf"] ?? false,
+              ),
+              ListTile(
+                title: Text("Flush image cache"),
+                subtitle: ValueBuilder(
+                  streamed: bloc.cacheSize,
+                  builder: (context, snapshot) {
+                    return Text(
+                      "Flushes all images from the cache (${snapshot.data} Mb)",
                     );
                   },
-                )
-              ],
-            );
-          }
-        ));
+                ),
+                leading: Icon(
+                  Icons.cached,
+                  size: 48.0,
+                ),
+                onTap: () {
+                  showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        title: Text("Warning"),
+                        content: Text(
+                          "Are you sure you want to clear the image cache? This cannot be undone.",
+                        ),
+                        actions: <Widget>[
+                          FlatButton(
+                            child: Text("CANCEL"),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                          ),
+                          FlatButton(
+                            child: Text("CLEAR"),
+                            onPressed: () {
+                              bloc.clearCache();
+                              Navigator.of(context).pop();
+                            },
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
+              )
+            ],
+          );
+        },
+      ),
+    );
   }
 }
