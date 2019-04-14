bdfeec38dbc18b95123cb7c256817863f87265fb
videah <videah@selfish.systems>
2019-04-14 13:02:16 +0100

    add thumbnail preview to video post page

--- lib/widgets/post_content_video.dart
+++ lib/widgets/post_content_video.dart
@@ -1,5 +1,7 @@
 import 'package:chewie/chewie.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_advanced_networkimage/provider.dart';
+import 'package:flutter_advanced_networkimage/transition.dart';
 import 'package:msg_browser/api/models/post_list_item.dart';
 import 'package:pigment/pigment.dart';
 import 'package:video_player/video_player.dart';
@@ -38,11 +40,27 @@ class _PostContentVideoState extends State<PostContentVideo> {
 
   @override
   Widget build(BuildContext context) {
+    var height = widget.post.height *
+        MediaQuery.of(context).size.width /
+        widget.post.width;
+
     return Container(
-      color: Pigment.fromString("#284a81"),
-      // TODO: Hero tags are broken on Chewie, put one here
-      // when it gets fixed.
-      child: Chewie(controller: _chewieController),
-    );
+        color: Pigment.fromString("#284a81"),
+        // TODO: Hero tags are broken on Chewie, put one here
+        // when it gets fixed.
+        child: Stack(
+          children: <Widget>[
+            TransitionToImage(
+              fit: BoxFit.cover,
+              enableRefresh: true,
+              height: height,
+              image: AdvancedNetworkImage(
+                widget.post.previewUrl,
+                useDiskCache: true,
+              ),
+            ),
+            Chewie(controller: _chewieController),
+          ],
+        ));
   }
 }
