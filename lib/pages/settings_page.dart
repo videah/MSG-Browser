8a216b470dbcbc92b330dcb6de875001a0bbbf37
videah <videah@selfish.systems>
2019-05-01 21:42:58 +0100

    remove elevation from settings

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -16,6 +16,7 @@ class _SettingsPageState extends State<SettingsPage> {
     return Scaffold(
       backgroundColor: Theme.of(context).canvasColor,
       appBar: AppBar(
+        elevation: 0.0,
         title: Text("Settings"),
       ),
       body: PersistedStateBuilder(
