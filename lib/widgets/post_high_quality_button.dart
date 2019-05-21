b6b8c2477445cb6fce09ff0a0274db547fffce71
videah <videah@selfish.systems>
2019-05-21 04:04:54 +0100

    have hq button display status

--- lib/widgets/post_high_quality_button.dart
+++ lib/widgets/post_high_quality_button.dart
@@ -5,11 +5,20 @@ import 'package:msg_browser/blocs/post_bloc.dart';
 class PostHighQualityButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-      icon: Icon(Icons.hd),
-      tooltip: "Toggle HD Version",
-      onPressed: () {
-        BlocProvider.of<PostBloc>(context).toggleHighQuality.add(true);
+    return StreamBuilder(
+      stream: BlocProvider.of<PostBloc>(context).highQuality,
+      builder: (context, snapshot) {
+        var color = snapshot.data == true
+            ? Theme.of(context).accentColor
+            : Theme.of(context).iconTheme.color;
+        return IconButton(
+          icon: Icon(Icons.hd),
+          tooltip: "Toggle High Quality",
+          color: color,
+          onPressed: () {
+            BlocProvider.of<PostBloc>(context).toggleHighQuality.add(true);
+          },
+        );
       },
     );
   }
